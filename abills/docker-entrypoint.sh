76f99a087ea40c7ee0fbdde662e9a7884927e7d4
huzdenov <hadji.uzdenov@gmail.com>
2024-07-08 18:49:01 +0400

    update Dockerfile

--- abills/docker-entrypoint.sh
+++ abills/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-if ! [ -f /usr/abills/libexec/config.pl]; then
-    exit 1;
-else
-    exec "$@"
+if ! [ -f /usr/abills/libexec/config.pl ]; then
+    cp /usr/abills/libexec/config.pl.default /usr/abills/libexec/config.pl;
+    sed -i "s/localhost/${DB_HOST}/g" /usr/abills/libexec/config.pl;
+    sed -i "s/sqlpassword/${DB_PASSWORD}/g" /usr/abills/libexec/config.pl;
+    sed -i "s/test12345678901234567890/${SECRETKEY}/g" /usr/abills/libexec/config.pl
 fi
+
+exec "$@"
